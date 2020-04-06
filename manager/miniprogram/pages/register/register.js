ddf9709647090aa652dff4dde75d4fc87e45e30c
hyf <hyf@cyydeMacBook-Air.local>
2020-04-06 14:21:53 +0800

    first commit

--- manager/miniprogram/pages/register/register.js
+++ manager/miniprogram/pages/register/register.js
@@ -11,7 +11,36 @@ Page({
     takeSession: false,
     requestResult: ''
   },
-
+  saoma: function () {
+    var that = this;
+    var show;
+    // 允许从相机和相册扫码
+    wx.scanCode({
+      success: (res) => {
+        console.log(res)
+        this.show = "result:" + res.result + " type" + res.scanType;
+        that.setData({
+          show: this.show,
+          name: res.result,
+          address: res.charSet
+        })
+        wx.showToast({
+          title: 'success',
+          icon: 'sc',
+          duration: 2000
+        })
+        var i = 0;
+        while (1) {
+          i++;
+          if (i == 1500000000) break;
+        }
+        this.saoma()
+      },
+      fail: (res) => {
+        console.log(res)
+      },
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
