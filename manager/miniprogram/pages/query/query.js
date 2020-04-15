a9e1537e68b34078e04065945cf46c5ceececb4e
ZJ Zhou <1073242461@qq.com>
2020-04-15 19:13:07 +0800

    sssss

--- manager/miniprogram/pages/query/query.js
+++ manager/miniprogram/pages/query/query.js
@@ -3,15 +3,16 @@ Page({
    * 初始化数据
    */
   data: {
-    build: '',
+    building: '',
     room: '',
     unit: '',
     name:'',
+    queryResult:'',
   },
 
 
   searchbuilding: function (e) {
-    this.data.build = e.detail.value;
+    this.data.building = e.detail.value;
 
   },
 
@@ -31,12 +32,79 @@ Page({
 
   searchname: function(){
     console.log('姓名：',this.data.name);
+    const db = wx.cloud.database()
+    db.collection('person').where({
+      name:this.data.name
+    })
+    .get()
+    .then(res=> {
+      if(res.data.length==0){
+        console.log('no ralated records')
+      }
+      else{
+        this.setData({
+          queryResult: JSON.stringify(res.data)
+        })
+        console.log('success to query')
+        console.log(res.data)
+      }
+    })
   },
 
   chaxun: function () {
-    console.log('楼号: ', this.data.build);
+    console.log('楼号: ', this.data.building);
     console.log('单元', this.data.unit);
     console.log('室: ', this.data.room);
+
+    const db = wx.cloud.database()
+    db.collection('person').where({
+      building:parseInt(this.data.building),
+      unit:parseInt(this.data.unit),
+      room:parseInt(this.data.room)
+    })
+      .get()
+      .then(res => {
+        if (res.data.length == 0) {
+          console.log('no ralated records')
+        }
+        else {
+          this.setData({
+            queryResult: JSON.stringify(res.data)
+          })
+          console.log('success to query')
+          console.log(res.data)
+        }
+      })
+
+    // const db=wx.cloud.database()
+    // db.collection('person').where({
+    //   building:this.data.build,
+    // })
+    // .get()
+    // .then(res=>{
+    //   if(res.data.length==0){
+    //     console.log('no related data')
+    //   }
+    //   else{
+    //     console.log('success to query')
+    //     console.log(res.data)
+    //   }
+
+    // })
+
+
+
+    // ({
+    //   success:res=>{
+    //     this.setData({
+    //       queryResult:JSON.stringify(res.data,null,2)
+    //     })
+    //     console.log('successquery',res)
+    //   },
+    //   fail:err=>{
+    //     console.log('failed')
+    //   }
+    // })
   },
 
   onLoad: function (options) {
