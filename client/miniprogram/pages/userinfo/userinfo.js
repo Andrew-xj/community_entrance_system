7171257904dc3ad2505f5ac0d543d2aa113c089f
Andrew-xj <544876317@qq.com>
2020-04-30 23:31:01 +0800

    userinfo 100% (qrcode finished)

--- client/miniprogram/pages/userinfo/userinfo.js
+++ client/miniprogram/pages/userinfo/userinfo.js
@@ -2,6 +2,9 @@
 
 const db = wx.cloud.database()
 const _ = db.command
+var QRCode = require('../../utils/qr-core.js')
+var qrcode = null;
+var that;
 
 Page({
 
@@ -18,40 +21,113 @@ Page({
       room: null
     },
     tag: true,
-    docID: ""
+    docID: "",
+    fileID: '',
+    qrcodeWidth: 0,
   },
 
   save: function (e) {
+    that = this
+    const ctx = wx.createCanvasContext('canvas')
+    const rate = wx.getSystemInfoSync().windowWidth / 750
+    var qrcodeWidth = rate * 400
+    that.setData({
+      qrcodeWidth: qrcodeWidth
+    })
+    qrcode = new QRCode('canvas', {
+      usingIn: that,
+      width: qrcodeWidth,
+      height: qrcodeWidth,
+      colorDark: '#000000',
+      colorLight: "white",
+      correctLevel: QRCode.CorrectLevel.H
+    })
     if (this.data.tag) {
       if (this.data.name != "" &&
         this.data.phone != null &&
         this.data.neighbourhood != "" &&
         this.data.address.block != null &&
         this.data.address.room != null) {
-        db.collection('resident').add({
-          data: {
-            "name": this.data.name,
-            "phone": this.data.phone,
-            "neighbourhood": this.data.neighbourhood,
-            "address": {
-              "block": this.data.address.block,
-              "unit": this.data.address.unit,
-              "room": this.data.address.room
-            },
-            "tag": false
-          },
-          success: function (res) {
-            wx.showToast({
-              title: '信息保存成功',
-              icon: 'success',
-              duration: 2000
-            })
-          }
+        // 生成二维码
+        var content = "姓名：" + that.data.name
+          + "\n手机：" + that.data.phone
+          + "\n小区：" + that.data.neighbourhood
+          + "\n详细住址：" + that.data.address.block + " 栋 " + that.data.address.unit + " 单元 " + that.data.address.room + " 室"
+        var fileName = that.data.name + '_' + that.data.phone + '.png'
+        var ImagePath
+        qrcode.makeCode(content)
+        qrcode.exportImage(function (path) {
+          console.log('path : ' + path)
+          ImagePath = path
+          // 上传存储器
+          wx.cloud.uploadFile({
+            cloudPath: fileName,
+            filePath: ImagePath,
+            success: res => {
+              that.setData({
+                fileID: res.fileID
+              })
+              // 上传数据库
+              db.collection('resident').add({
+                data: {
+                  "name": that.data.name,
+                  "phone": that.data.phone,
+                  "neighbourhood": that.data.neighbourhood,
+                  "address": {
+                    "block": that.data.address.block,
+                    "unit": that.data.address.unit,
+                    "room": that.data.address.room
+                  },
+                  "tag": false,
+                  "fileID": that.data.fileID
+                },
+                success: function (res) {
+                  wx.showToast({
+                    title: '信息保存成功',
+                    icon: 'success',
+                    duration: 2000
+                  })
+                  setTimeout(function () {
+                    wx.reLaunch({
+                      url: '../userinfo/userinfo',
+                    })
+                  }, 1000)
+                }
+              })
+            }
+          })
         })
       }
     }
     else {
-      db.collection('resident').doc(this.data.phone).set({
+      // 生成二维码
+      var content = "姓名：" + that.data.name
+        + "\n手机：" + that.data.phone
+        + "\n小区：" + that.data.neighbourhood
+        + "\n详细住址：" + that.data.address.block + " 栋 " + that.data.address.unit + " 单元 " + that.data.address.room + " 室"
+      var fileName = that.data.name + '_' + that.data.phone + '.png'
+      var ImagePath;
+      qrcode.makeCode(content)
+      qrcode.exportImage(function (path) {
+        ImagePath = path
+        wx.cloud.deleteFile({
+          fileList: [that.data.fileID],
+          success: res => {
+          },
+          fail: console.error
+        })
+        wx.cloud.uploadFile({
+          cloudPath: fileName,
+          filePath: ImagePath,
+          success: res => {
+            that.setData({
+              fileID: res.fileID
+            })
+          }
+        })
+      })
+      // 更新二维码图片
+      db.collection('resident').doc(this.data.docID).set({
         data: {
           "name": this.data.name,
           "phone": this.data.phone,
@@ -61,7 +137,8 @@ Page({
             "unit": this.data.address.unit,
             "room": this.data.address.room
           },
-          "tag": false
+          "tag": false,
+          "fileID": this.data.fileID
         },
         success: function (res) {
           wx.showToast({
@@ -69,35 +146,48 @@ Page({
             icon: 'success',
             duration: 2000
           })
+          setTimeout(function () {
+            wx.reLaunch({
+              url: '../userinfo/userinfo',
+            })
+          }, 1000)
         }
       })
     }
   },
 
   qr: function (e) {
-
+    wx.navigateTo({
+      url: 'info/info?fileID=' + this.data.fileID,
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const rate = wx.getSystemInfoSync().windowWidth / 750
+    var qrcodeWidth = rate * 400
+    this.setData({
+      qrcodeWidth: qrcodeWidth
+    })
     var that = this
     db.collection('resident').where({
       phone: _.neq("")
     }).get({
-      success: function(res){
+      success: function (res) {
         that.setData({
           name: res.data[0].name,
           phone: res.data[0].phone,
           neighbourhood: res.data[0].neighbourhood,
           tag: res.data[0].tag,
           docID: res.data[0]._id,
-          address:{
+          address: {
             block: res.data[0].address.block,
             unit: res.data[0].address.unit,
             room: res.data[0].address.room
-          }
+          },
+          fileID: res.data[0].fileID
         })
       }
     })
@@ -172,7 +262,7 @@ Page({
 
   block: function (e) {
     this.setData({
-      address:{
+      address: {
         block: e.detail.value,
         unit: this.data.address.unit,
         room: this.data.address.room
