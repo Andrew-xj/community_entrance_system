1ff9a5f97bddc307a398bf6bf4c4463457da5621
Andrew-xj <544876317@qq.com>
2020-05-16 10:32:39 +0800

    communication finished

--- manager/miniprogram/pages/scan/scan.js
+++ manager/miniprogram/pages/scan/scan.js
@@ -1,14 +1,17 @@
 var app = getApp()
-var hellodata = {
-  name: '',
-  address: '',
-  time_left: ''
-}
+var scanresult
 
 Page({
   data: {
-    show: "",
-    hellodata
+    date:'',
+    time:'',
+    name:'',
+    phonenumber:'',
+    address:'',
+    access:'',
+    identifier:'',
+    inandout:'',
+    counter:null,
   },
 
 
@@ -20,12 +23,29 @@ Page({
     wx.scanCode({
       success: (res) => {
         console.log(res)
-        this.show = "result:" + res.result + " type" + res.scanType;
+        scanresult = JSON.stringify(res.result).split("\\n")
         that.setData({
-          show: this.show,
-          name: res.result,
-          address: res.charSet
+          show:this.show,
+          
+          date: scanresult[0].replace(/\"/g, ""),
+          time: scanresult[1],
+          name: scanresult[2],
+          phonenumber: scanresult[3],
+          address: scanresult[4],
+          access: scanresult[5],
+          identifier: scanresult[6],
+          inandout: scanresult[7].replace(/\"/g, ""),
+          //name: JSON.stringify(res.result).split('\n'),//res.result.split(//\r),
+          //[data,time,name,phonenumber,address,access,identifier]:JSON.stringify(res.result).split("\n"),
         })
+        if(that.data.inandout.slice(9)=='out'){
+          that.addrecord()
+        }
+        else if(that.data.inandout.slice(9)=='in'){
+          console.log('jud')
+          that.updaterecord()
+        }
+        this.querytoday()
         wx.showToast({
           title: 'success',
           icon: 'sc',
@@ -41,6 +61,68 @@ Page({
       fail: (res) => {
         console.log(res)
       },
+    })
+  },
+  querytoday:function(){
+    const db=wx.cloud.database()
+    db.collection('date').where({
+      date: scanresult[0].slice(6)
+    }).get({
+      success:function(res){
+        console.log(res)
+        console.log(scanresult[0].slice(6))
+      }
+    })
+  },
+  adddays:function(){
+    var curdate = new Date().toLocaleDateString();
+    const db=wx.cloud.database()
+    const _=db.command
+    db.collection('date').doc(scanresult[0].slice(6)).update({
+      data:{
+        count:5
+      },
+      success:function(res){
+        console.log('success to update')
+      }
+    })
+  },
+  updaterecord:function(){
+    const db=wx.cloud.database()
+    db.collection('records').where({
+      identifier: this.data.identifier.slice(12)
+    }).get({
+      success:(res)=>{
+        db.collection('records').doc(res.data[0]._id).update({
+          data:{
+            inandout:'in'
+          },
+          success:(res)=>{
+            console.log('suc to update a in-code')
+          }
+        })
+      }
+    })
+  },
+  addrecord:function(){
+    const db=wx.cloud.database()
+    db.collection('records').add({
+      data:{
+        date: scanresult[0].slice(6),
+        time: scanresult[1].slice(4),
+        name: scanresult[2].slice(4),
+        phonenumber: scanresult[3].slice(4),
+        address: scanresult[4].slice(4),
+        access: scanresult[5].slice(6),
+        identifier: this.data.identifier.slice(12),
+        inandout:this.data.inandout.slice(9)
+      },
+      success:function(res){
+        console.log('success to upload a record')
+      },
+      fail: function (res) {
+        console.log('fail to upload a record')
+      }
     })
   }
 })
--- manager/miniprogram/pages/statistics/statistics.js
+++ manager/miniprogram/pages/statistics/statistics.js
@@ -1,7 +1,7 @@
 var wxCharts = require("../../util/wxcharts-min.js");
 
 //定义记录初始屏幕宽度比例，便于初始化
-
+var previousdate = new Array(7);
 
 var windowW = 0;
 
@@ -11,10 +11,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    
   },
 
-
+  adddays:function(dateStr,dayCount){
+    var strdate = dateStr;
+    var isdate = new Date(strdate.replace(/-/g, "/"));
+    isdate = new Date((isdate / 1000 + (86400 * dayCount)) * 1000);  
+    var pdate = isdate.getFullYear() + "/" + (isdate.getMonth() + 1) + "/" + (isdate.getDate());
+    return pdate;
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -32,6 +38,24 @@ Page({
     windowW = this.data.imageWidth / 375;
     console.log(windowW);
 
+    //var date1 = Date.parse('2020/05/14');
+    // var currenttimestamp = new Date().getTime();
+    // var idays;
+    // idays=Math.floor((currenttimestamp-date1)/(24*3600*1000));
+    // console.log(idays);
+    var mydate=new Date().toLocaleDateString();
+    console.log(mydate)
+    mydate='2020/3/2';
+    //console.log(mydate.replace(/-/g,"/"));
+    //var isdate=Date.parse(mydate);
+    //console.log(isdate);
+
+
+    for (var i=0;i<7;i++){
+      previousdate[i]=this.adddays(mydate,i-7);
+      previousdate[i]=previousdate[i].slice(5);
+    }
+    console.log(previousdate);
   },
 
   /**
@@ -45,15 +69,12 @@ Page({
       canvasId: 'columnCanvas',
       type: 'line',
       animation: true,
-      categories: [1,2,3,4,5,6,7],
+      categories: previousdate,
       series: [{
         name: '人数',
         data: [65,54,86,71,56,93,20],
         
       }, ],
-      title:{
-        name:'出入人数统计',
-      },
       yAxis: {
         format: function (val) {
           return val ;
@@ -65,11 +86,11 @@ Page({
         disableGrid: false,
         type: 'calibration'
       },
-      extra: {
-        column: {
-          width: 15
-        }
-      },
+      // extra: {
+      //   column: {
+      //     width: 15
+      //   }
+      // },
       width: (375 * windowW),
       height: (400 * windowW),
     });
