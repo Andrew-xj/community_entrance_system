7fc9df45b8dd54f8e89002736f02b455b69aa6f6
Andrew-xj <544876317@qq.com>
2020-05-21 23:32:25 +0800

    qrcode page logic finished - 100%

--- client/miniprogram/pages/qrcode/qrcode.js
+++ client/miniprogram/pages/qrcode/qrcode.js
@@ -7,6 +7,7 @@ var QRCode = require('../../utils/qr-core.js');
 var qrcode = null;
 var that;
 var upload = false;
+var threshold = 3;
 
 Page({
 
@@ -18,9 +19,10 @@ Page({
     qrcodeWidth: 0,
     rest: 0,
     info: null,
-    canOut: true,
+    canOut: '',
     identifier: '',
     access_token: '',
+    rest:''
   },
 
   /**
@@ -28,7 +30,6 @@ Page({
    */
   onLoad: function (options) {
     that = this;
-
     db.collection('resident').where({
       phone: _.neq("")
     }).get({
@@ -36,78 +37,165 @@ Page({
         that.setData({
           info: res.data[0],
         })
+        var days = that.getDays();
         db.collection('ExitAndEnter').where({
-          Exit_or_Enter: "Exit"
+          Exit_or_Enter: 'Enter',
+          phone: res.data[0].phone,
+          date: _.in(days)
         }).get({
-          success: (res) => {
-            if (res.data.length != 0) {
+          success:(res)=>{
+            if(threshold-res.data.length > 0){
               that.setData({
-                action: false
+                canOut: true,
+                // rest: threshold-res.data.length
+                rest:res.data.length
               })
-              var identifier = res.data[0].identifier;
-              var time = res.data[0].time;
-              var date = res.data[0].date;
-              var info = that.data.info;
-              var address = res.data[0].address;
-              var content =
-                '日期: ' + date + '\n时间: ' + time + '\n姓名: ' + info.name +
-                '\n手机: ' + info.phone + '\n住址: ' + address +
-                "\n能否出门: " + that.data.canOut + '\nidentifier: ' + identifier + 
-                "\ninorout: in";
-              that.createqrcode(content);
-              setInterval(() => {
-                setTimeout(() => {
-                  that.lookup(identifier, info, date, time, address);
-                }, 5000);
-              }, 20000);
-            } else {
+            }else{
               that.setData({
-                action: true
+                canOut: false,
+                // rest: threshold-res.data.length
+                rest: res.data.length
               })
-              var identifier = that.createNonceStr();
-              var time = util.formatTime(new Date());
-              var date = time.split(' ')[0];
-              time = time.split(' ')[1];
-              var info = that.data.info;
-              var address = info.neighbourhood;
-              if (info.address.block != null) {
-                address += info.address.block + '栋';
-              }
-              if (info.address.unit != null) {
-                address += info.address.unit + '单元'
-              }
-              if (info.address.room != null) {
-                address += info.address.room + '室'
-              }
-              var content =
-                '日期: ' + date + '\n时间: ' + time + '\n姓名: ' + info.name +
-                '\n手机: ' + info.phone + '\n住址: ' + address +
-                "\n能否出门: " + that.data.canOut + '\nidentifier: ' + identifier + 
-                '\ninorout: out';
-              that.createqrcode(content);
-              setTimeout(() => {
-                that.lookup(identifier, info, date, time, address);
-              }, 60000);
-              setInterval(() => {
-                identifier = that.createNonceStr();
-                time = util.formatTime(new Date());
-                date = time.split(' ')[0];
-                time = time.split(' ')[1];
-                content =
-                  '日期: ' + date + '\n时间: ' + time + '\n姓名: ' + info.name +
-                  '\n手机: ' + info.phone + '\n住址: ' + address +
-                  "\n能否出门: " + that.data.canOut + '\nidentifier: ' + identifier +
-                  '\ninorout: out';
-                that.createqrcode(content);
-                setTimeout(() => {
-                  that.lookup(identifier, info, date, time, address)
-                }, 60000);
-              }, 600000);
             }
-          },
+            db.collection('ExitAndEnter').where({
+              Exit_or_Enter: "Exit"
+            }).get({
+              success: (res) => {
+                if (res.data.length != 0) {
+                  that.setData({
+                    action: false
+                  })
+                  var identifier = res.data[0].identifier;
+                  var time = res.data[0].time;
+                  var date = res.data[0].date;
+                  var info = that.data.info;
+                  var address = res.data[0].address;
+                  var content =
+                    '日期: ' + date + '\n时间: ' + time + '\n姓名: ' + info.name +
+                    '\n手机: ' + info.phone + '\n住址: ' + address +
+                    "\n能否出门: " + that.data.canOut + '\nidentifier: ' + identifier + 
+                    "\ninorout: in";
+                  that.createqrcode(content);
+                  setInterval(() => {
+                    setTimeout(() => {
+                      that.lookup(identifier, info, date, time, address);
+                    }, 5000);
+                  }, 20000);
+                } else {
+                  that.setData({
+                    action: true
+                  })
+                  var identifier = that.createNonceStr();
+                  var time = util.formatTime(new Date());
+                  var date = time.split(' ')[0];
+                  time = time.split(' ')[1];
+                  var info = that.data.info;
+                  var address = info.neighbourhood;
+                  if (info.address.block != null) {
+                    address += info.address.block + '栋';
+                  }
+                  if (info.address.unit != null) {
+                    address += info.address.unit + '单元'
+                  }
+                  if (info.address.room != null) {
+                    address += info.address.room + '室'
+                  }
+                  var content =
+                    '日期: ' + date + '\n时间: ' + time + '\n姓名: ' + info.name +
+                    '\n手机: ' + info.phone + '\n住址: ' + address +
+                    "\n能否出门: " + that.data.canOut + '\nidentifier: ' + identifier + 
+                    '\ninorout: out';
+                  that.createqrcode(content);
+                  setTimeout(() => {
+                    that.lookup(identifier, info, date, time, address);
+                  }, 5000);
+                  setInterval(() => {
+                    identifier = that.createNonceStr();
+                    time = util.formatTime(new Date());
+                    date = time.split(' ')[0];
+                    time = time.split(' ')[1];
+                    content =
+                      '日期: ' + date + '\n时间: ' + time + '\n姓名: ' + info.name +
+                      '\n手机: ' + info.phone + '\n住址: ' + address +
+                      "\n能否出门: " + that.data.canOut + '\nidentifier: ' + identifier +
+                      '\ninorout: out';
+                    that.createqrcode(content);
+                    setInterval(() => {
+                      that.lookup(identifier, info, date, time, address)
+                    }, 5000);
+                  }, 20000);
+                }
+              },
+            })
+          }
         })
+        
       }
     })
+  },
+
+  getDays: function() {
+    var days = [];
+    var day1 = util.formatTime(new Date()).split(' ')[0];
+    days.push(day1);
+    var ri = day1.substr(-2);
+    var yue = day1.substr(-5,2);
+    var nian = Number(day1.substr(1,4));
+    if (Number(ri) == 1) {
+      if(Number(yue) in [1,2,4,6,8,9,11]){
+        ri = '31';
+        days.push(day1.substr(0,8)+ri);
+        ri = '30';
+        days.push(day1.substr(0,8)+ri);
+      }else if(Number(yue) in [5,7,10,12]){
+        ri = '30';
+        days.push(day1.substr(0,8)+ri);
+        ri = '29';
+        days.push(day1.substr(0,8)+ri);
+      }else{
+        if((nian%4 != 0)||((nian%100 == 0) && (nian&400 != 0))){
+          ri = '28';
+          days.push(day1.substr(0,8)+ri);
+          ri = '27';
+          days.push(day1.substr(0,8)+ri);
+        }else{
+          ri = '29';
+          days.push(day1.substr(0,8)+ri);
+          ri = '28';
+          days.push(day1.substr(0,8)+ri);
+        }
+      }
+    }else if(Number(ri) == 2){
+      if(Number(yue) in [1,2,4,6,8,9,11]){
+        ri = '1';
+        days.push(day1.substr(0,8)+ri);
+        ri = '31';
+        days.push(day1.substr(0,8)+ri);
+      }else if(Number(yue) in [5,7,10,12]){
+        ri = '1';
+        days.push(day1.substr(0,8)+ri);
+        ri = '30';
+        days.push(day1.substr(0,8)+ri);
+      }else{
+        if((nian%4 != 0)||((nian%100 == 0) && (nian&400 != 0))){
+          ri = '1';
+          days.push(day1.substr(0,8)+ri);
+          ri = '28';
+          days.push(day1.substr(0,8)+ri);
+        }else{
+          ri = '1';
+          days.push(day1.substr(0,8)+ri);
+          ri = '29';
+          days.push(day1.substr(0,8)+ri);
+        }
+      }
+    }else{
+      ri = Number(ri)-1;
+      days.push(day1.substr(0,8)+ri.toString());
+      ri = ri - 1;
+      days.push(day1.substr(0,8)+ri.toString());
+    }
+    return days;
   },
 
   createqrcode: function (content) {
